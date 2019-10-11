dea4e1fefbdfe22eeabfd655c3486abd3504c28b
Stephen Hill <stephen.hill9@nhs.net>
2019-10-11 12:16:48 +0100

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,44 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+module.exports = router
+
 // add your routes here
 
-module.exports = router
+router.get(/benefit-handler/, function (req, res) {
+
+  var benefitList = req.session.data.benefitList
+
+  // If no array exists, create one called 'benefitList'. If one already exists, do nothing.
+
+  benefitList = ( typeof benefitList != 'undefined' && benefitList instanceof Array ) ? benefitList : []
+
+  // Create a variable of the posted information
+
+  var benefitName = req.session.data['benefitname'];
+
+  // Add the posted information into the 'benefitList' array
+
+  benefitList.push(benefitName);
+
+  req.session.data.benefitList = benefitList;
+
+  console.log(benefitList)
+
+  console.log('Benefits list contains', benefitList.length, 'items')
+
+  // Redirect to the 'Do you get another?' page
+
+  res.redirect('answers');
+
+});
+
+router.get(/addanother-handler/, function (req, res) {
+  if (req.query.addanother == 'yes') {
+    res.redirect('input');
+  } if (req.query.addanother == 'no') {
+    res.redirect('done');
+  } else {
+    res.redirect('answers');
+  }
+});
